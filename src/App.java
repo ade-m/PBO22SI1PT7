84f1dca6b95af35bf0b3be4369d57297259deb5a
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-12 09:54:32 +0700

    Dependency

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import model.Engine;
 import model.Mobil;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        //dependency
         Engine engine = new Engine();
         Mobil mobil = new Mobil();
         Engine engine2 = new Engine();
